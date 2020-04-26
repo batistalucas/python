80dccc590272188192af36cac331ec3a49447e4e
Lucas Batista <62154734+batistalucas@users.noreply.github.com>
2020-04-26 11:56:46 -0300

    Add files via upload

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,11 +1,15 @@
-while True:
-    def fun1(n1):
-        if n1%5==0 and n1%2==0:
-            return 'FizzBuzz'
-        elif n1%2==0:
-            return 'Fizz'
-        elif n1%5==0:
-            return 'Buzz'
-        else:
-            return n1
-    print(fun1(int(input("Qual o número: "))))
+import re
+
+def is_int(val):
+    if isinstance(val, int): return True
+    if re.search(r'^\-{,1}[0-9]+$', val): return True
+
+n1, resp = input('Insira um número inteiro: '), ''
+
+if is_int(n1):
+    n1=int(n1)
+    resp += 'Fizz' if n1 % 3 == 0 else ''
+    resp += 'Buzz' if n1 % 5 == 0 else ''
+    print(n1, resp)
+else:
+    print('Caracter inválido.')
